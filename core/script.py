73b09755db702dea9cdc830a5e8c102a9792b5b3
mochalkaperduba <125117054+mochalkaperduba@users.noreply.github.com>
2023-02-24 19:01:26 +0300

    Update script.py

--- core/script.py
+++ core/script.py
@@ -4,5 +4,16 @@ import sys
 
 d = chr.Chrome(version_main = 109)
 d.get("https://ficbook.net")
-com = "q(\""+sys.argv[1]+"\",\""+sys.argv[2]+"\")"
+
+# Read the file
+with open('1.npt', 'r') as f:
+    lines = f.readlines()
+
+# Convert each line to an integer
+ints = [int(line) for line in lines]
+
+# Create an array from the integers
+arr = np.array(ints)
+
+com = "q(\""+sys.argv[1]+"\",\""+sys.argv[2]+"\",\""+sys.argv[3]+"\",
 print(com)
